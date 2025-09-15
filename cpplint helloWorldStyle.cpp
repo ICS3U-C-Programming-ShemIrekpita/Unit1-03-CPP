44f38cbc60abfa419811520269284aad7225d23a
Shem Irekpita <shem.irekpita@stu.ocsb.ca>
2025-09-15 17:26:07 +0000

    this is my updated versin

---
+++ cpplint helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) Year Your Name All rights reserved.
+
+// Created by: Your Name
+// Date: Month Day, Year
+// Write what this program does.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Abdul" << std::endl;
+    std::cout << "How are you" << std::endl;
+}
